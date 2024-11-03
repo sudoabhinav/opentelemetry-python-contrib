52b23c7e480aa351a5e109cb6deb5f605c46dbad
Abhinav Singh <abhinav.singh01@credgenics.com>
2024-11-03 20:37:49 +0530

    fix typo in name

--- instrumentation/opentelemetry-instrumentation-pymongo/src/opentelemetry/instrumentation/pymongo/utils.py
+++ instrumentation/opentelemetry-instrumentation-pymongo/src/opentelemetry/instrumentation/pymongo/utils.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-COMMAND_TO_ATTRIBUTES_MAPPING = {
+COMMAND_TO_ATTRIBUTE_MAPPING = {
     "insert": ["documents"],
     "delete": ["deletes"],
     "update": ["updates"],
